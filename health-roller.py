352034cd163b2f68605d69be6e95162fab460111
AugmenTab <tbaum92@live.com>
2020-06-06 14:36:53 -0600

    Added function names for calculating based on different calcMethods.

--- health-roller.py
+++ health-roller.py
@@ -9,12 +9,26 @@ def getHealth(hitDice, method):
 
 #TODO: Write a parser for the regular expression.
 
+def calcAvg():
+    pass # 1/2 die value rounded down for every roll.
+
+def calcMax():
+    pass # Max die value for every roll.
+
+def calcMin():
+    pass # Min die value for every roll (1).
+
+def rollHealth():
+    pass # Random rolls.
+
+def calcSug():
+    pass # (1/2)+1 die value rounded down for every roll.
+
 print("Enter the monster's hit dice.")
 hitDice = str(input())
 
 print("""Enter how you would like health calculated:
     - avg for the average health.
-    - sug for the suggested health.
     - max for the maximum health.
     - min for the minimum health.
     - rol for randomly rolled health.
